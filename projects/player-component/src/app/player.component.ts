650476a159c28cc08a98be237bb0b7ab7830b2de
rhenck <richard.henck@iqb.hu-berlin.de>
2021-01-22 15:00:52 +0100

    Refactor player startData

--- projects/player-component/src/app/player.component.ts
+++ projects/player-component/src/app/player.component.ts
@@ -20,34 +20,17 @@ import { DataService } from './data.service';
 })
 export class PlayerComponent {
   @Output() valueChanged = new EventEmitter<string>();
-  private _startData: StartData = {
-    unitDefinition: '',
-    unitState: {
-      dataParts: {}
-    }
-  };
+  // @Output() ready = new EventEmitter();
 
   form = new FormGroup({});
 
   constructor(public dataService: DataService) {}
 
-  get startData(): StartData {
-    return this._startData;
-  }
-
   @Input()
-  set startData(data: StartData) {
-    this.setStartData(data);
-  }
-
-  // @Output() ready = new EventEmitter();
-
-  private setStartData(startData: StartData): void {
-    this._startData = startData;
+  set startData(startData: StartData) {
     if (startData.unitDefinition) {
       let storedResponses = {};
       if (startData.unitState && startData.unitState.dataParts) {
-        console.log('unitState.dataParts', startData.unitState.dataParts);
         const storedResponsesRaw = startData.unitState.dataParts;
         if (storedResponsesRaw && storedResponsesRaw.allResponses) {
           storedResponses = JSON.parse(storedResponsesRaw.allResponses);
